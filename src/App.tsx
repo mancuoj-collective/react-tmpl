734136fb6662ee56d0d87b5dbba991c3a930469b
mancuoj <1337381768@qq.com>
2023-10-21 21:03:44 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold font-serif text-5xl text-center text-green-600">
+        React Tmpl
+      </h1>
+    </>
+  )
+}
+
+export default App
