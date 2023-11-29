dc30a9ff4acbb51efc099a03df55f3e998601401
mancuoj <1337381768@qq.com>
2023-11-29 20:45:48 +0800

    feat: theme provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
+import { ThemeProvider } from '@/components/theme-provider'
+
 export default function App() {
   return (
-    <>
-      <h1 className="h-screen flex justify-center items-center gap-5 font-bold font-serif text-3xl text-blue-600">
+    <ThemeProvider>
+      <div className="h-screen flex justify-center items-center gap-4 bg-slate-100 text-blue-600 dark:bg-slate-900 dark:text-blue-300">
         <span className="i-carbon-logo-react text-5xl"></span>
-        React Tmpl
-      </h1>
-    </>
+        <h1 className="font-bold font-serif text-3xl ">React Tmpl</h1>
+      </div>
+    </ThemeProvider>
   )
 }
