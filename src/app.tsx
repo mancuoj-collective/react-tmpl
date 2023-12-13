2b640431c2386af5d673182917a1659b39ca6e80
mancuoj <1337381768@qq.com>
2023-12-13 23:35:11 +0800

    chore: remove daisyui

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
+import { ThemeProvider } from '@/components/theme-provider'
 
 export default function App() {
   const [text, setText] = useState('React')
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-5 font-serif select-none">
-      <h1 className="font-bold text-3xl bg-clip-text text-transparent bg-gradient-to-r from-rose-900 to-teal-900 dark:from-rose-100 dark:to-teal-100">
-        Hello
-        {' '}
-        {text}
-      </h1>
-      <input type="text" className="input input-bordered max-w-xs w-full" value={text} onChange={(e) => { setText(e.target.value) }} />
-    </div>
+    <ThemeProvider>
+      <div className="h-screen flex flex-col justify-center items-center gap-5 font-serif">
+        <h1 className="font-bold text-3xl">
+          Hello
+          {' '}
+          {text}
+        </h1>
+        <input
+          className="max-w-xs w-full"
+          maxLength={12}
+          value={text}
+          onChange={(e) => { setText(e.target.value) }}
+        />
+      </div>
+    </ThemeProvider>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,25 +10,11 @@ export default {
     extend: {},
   },
   plugins: [
-    // https://github.com/egoist/tailwindcss-icons
     iconsPlugin({
       collections: getIconCollections(['lucide', 'carbon']),
     }),
-    // https://github.com/jamiebuilds/tailwindcss-animate
     require('tailwindcss-animate'),
-    // https://tailwindcss.com/docs/typography-plugin
     require('@tailwindcss/typography'),
-    // https://daisyui.com/docs/config
-    require('daisyui'),
+    require('@tailwindcss/forms'),
   ],
-  daisyui: {
-    themes: true,
-    darkTheme: 'black',
-    base: true,
-    styled: true,
-    utils: true,
-    prefix: '',
-    logs: true,
-    themeRoot: ':root',
-  },
 }
