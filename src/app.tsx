6fba98757c4fce9836a2bc0532b5087e2ab0b432
mancuoj <1337381768@qq.com>
2023-12-21 23:50:12 +0800

    chore: update

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,21 @@
 import type { FC } from 'react'
 import { useState } from 'react'
-import { ThemeProvider } from '@/components/theme-provider'
 
 const App: FC = () => {
-  const [text, setText] = useState('React')
+  const [text, setText] = useState('Hello React')
 
   return (
-    <ThemeProvider>
-      <div className="h-dvh flex flex-col justify-center items-center gap-5 font-serif">
-        <h1 className="font-bold text-3xl">
-          Hello
-          {' '}
-          {text}
-        </h1>
-        <input
-          className="max-w-xs w-full"
-          maxLength={12}
-          value={text}
-          onChange={(e) => { setText(e.target.value) }}
-        />
-      </div>
-    </ThemeProvider>
+    <div className="h-dvh flex flex-col justify-center items-center gap-5 font-serif overflow-hidden">
+      <h1 className="font-bold text-3xl">
+        {text}
+      </h1>
+      <input
+        className="max-w-xs w-full"
+        maxLength={20}
+        value={text}
+        onChange={(e) => { setText(e.target.value) }}
+      />
+    </div>
   )
 }
 
