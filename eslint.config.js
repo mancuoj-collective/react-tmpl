edae25980df1ec993096ac2e639f6d697478659b
mancuoj <1337381768@qq.com>
2023-12-27 21:23:47 +0800

    chore: mini+

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,9 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   react: true,
+  formatters: {
+    css: true,
+    html: true,
+    markdown: 'prettier',
+  },
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
