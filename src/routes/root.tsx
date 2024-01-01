fabde1a076dc52358e88f305f7276d460cf4ca9c
mancuoj <1337381768@qq.com>
2024-01-01 17:11:36 +0800

    style: custom css

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,10 @@
 import type { FC } from 'react'
 import { useState } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 
 const Root: FC = () => {
-  const [text, setText] = useState('Hello React')
+  const [text, setText] = useState('We will rock you')
+  const navigation = useNavigation()
 
   return (
     <div className="flex h-dvh flex-col items-center justify-center gap-5 overflow-hidden font-serif">
@@ -12,11 +13,14 @@ const Root: FC = () => {
         <span className="i-lucide-hand-metal"></span>
       </h1>
       <input
-        className="w-full max-w-xs border border-slate-800 px-3 py-2"
+        className="w-full max-w-sm border border-slate-800 px-3 py-2"
         value={text}
         onChange={e => setText(e.target.value)}
       />
-      <Outlet />
+
+      <div id="detail" className={navigation.state === 'loading' ? 'loading' : ''}>
+        <Outlet />
+      </div>
     </div>
   )
 }
