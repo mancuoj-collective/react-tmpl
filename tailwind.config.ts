b7bbfe130b9c00b95967646988c94257d935c7ac
mancuoj <1337381768@qq.com>
2024-01-20 23:21:28 +0800

    chore: migrate tailwind ts config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
-const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons')
+import { getIconCollections, iconsPlugin } from '@egoist/tailwindcss-icons'
+import type { Config } from 'tailwindcss'
 
-/** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -13,9 +13,8 @@ export default {
     },
   },
   plugins: [
-    require('tailwindcss-animate'),
     iconsPlugin({
       collections: getIconCollections(['lucide']),
     }),
   ],
-}
+} satisfies Config
