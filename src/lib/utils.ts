c52e8087da6105d4a2103dcb077390cd4fc891af
mancuoj <mancuoj@gmail.com>
2024-03-11 20:23:54 +0800

    chore: update deps

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
-import type { ClassValue } from 'clsx'
-import { clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import type { ClassValue } from 'ts-clsx'
+import { clsx } from 'ts-clsx'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(...inputs))
 }
