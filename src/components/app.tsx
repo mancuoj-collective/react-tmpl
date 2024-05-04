36017a3ff703b6b07807bd6d4456b369c45661ca
mancuoj <mancuoj@gmail.com>
2024-05-04 21:31:50 +0800

    feat: jotai dark

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,9 @@
 import { ThemeToggle } from './theme-toggle'
-import { ThemeProvider } from '~/components/theme-provider'
 
 export default function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="react-tmpl">
-      <div className="flex h-dvh flex-col items-center justify-center font-serif">
-        <ThemeToggle />
-      </div>
-    </ThemeProvider>
+    <div className="flex h-dvh items-center justify-center gap-3 font-serif">
+      <ThemeToggle />
+    </div>
   )
 }
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,36 +1,22 @@
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
+import { useDark } from '~/hooks/use-dark'
 import { Button } from '~/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '~/components/ui/dropdown-menu'
-import { useTheme } from '~/components/theme-provider'
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { toggleDark } = useDark()
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="ghost" size="icon" onClick={toggleDark}>
+      <span
+        role="img"
+        aria-hidden="true"
+        className="i-lucide-sun size-6 rotate-0 scale-100 transition-transform duration-500 dark:-rotate-90 dark:scale-0"
+      />
+      <span
+        role="img"
+        aria-hidden="true"
+        className="i-lucide-moon absolute size-6 rotate-90 scale-0 transition-transform duration-500 dark:rotate-0 dark:scale-100"
+      />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
