3fd03941eb5b023ddf05acb51989cfd789c7a2c4
mancuoj <mancuoj@gmail.com>
2024-05-04 19:51:16 +0800

    chore: update deps and clean

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,19 +1,9 @@
-import { useState } from 'react'
-
 export default function App() {
-  const [text, setText] = useState('We will rock you')
-
   return (
     <div className="flex h-dvh flex-col items-center justify-center gap-5 overflow-hidden font-serif">
-      <h1 className="flex items-center gap-5 text-3xl font-bold">
-        {text}
-        <span className="i-lucide-hand-metal"></span>
+      <h1 className="text-3xl font-bold">
+        Hello World
       </h1>
-      <input
-        className="w-full max-w-sm border border-foreground px-3 py-2"
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
     </div>
   )
 }
--- src/utils/cn.ts
+++
@@ -1,7 +0,0 @@
-import type { ClassValue } from 'ts-clsx'
-import { clsx } from 'ts-clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
