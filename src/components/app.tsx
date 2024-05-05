774276efd1814b8832bc559489020bc1a1783683
mancuoj <mancuoj@gmail.com>
2024-05-05 13:21:20 +0800

    chore: more ui components

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,11 @@
-import { ThemeToggle } from './theme-toggle'
+import { ThemeToggle } from '~/components/theme-toggle'
+import { Toaster } from '~/components/ui/sonner'
 
 export default function App() {
   return (
     <div className="flex h-dvh items-center justify-center gap-3 font-serif">
       <ThemeToggle />
+      <Toaster />
     </div>
   )
 }
