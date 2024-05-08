2d480e5d15ddfd2d0823bf33b05aceefb51c27a1
mancuoj <mancuoj@gmail.com>
2024-05-09 01:41:22 +0800

    feat: router link

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,10 +11,10 @@ function Root() {
     <div className="relative flex min-h-dvh flex-col font-sans antialiased">
       <div className="mx-auto p-20">
         <nav className="mb-10 flex items-center gap-3">
-          <Link to="/" className="text-foreground/75 [&.active]:text-foreground">
+          <Link to="/" className="font-bold text-foreground/60 [&.active]:text-foreground">
             Home
           </Link>
-          <Link to="/about" className="text-foreground/75 [&.active]:text-foreground">
+          <Link to="/about" className="font-bold text-foreground/60 [&.active]:text-foreground">
             About
           </Link>
           <ThemeToggle />
