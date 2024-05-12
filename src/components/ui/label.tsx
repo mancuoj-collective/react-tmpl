da47bb1c56fd8e0c0bbaf6bead9bb0ccb765ed64
mancuoj <mancuoj@gmail.com>
2024-05-13 00:48:29 +0800

    chore: update deps

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,7 +1,6 @@
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { type VariantProps, cva } from "class-variance-authority"
 import * as React from "react"
-
 import { cn } from "~/utils/cn"
 
 const labelVariants = cva("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70")
