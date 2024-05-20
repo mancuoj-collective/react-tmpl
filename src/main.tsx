4ae5d986a0dee9865d26172d53c29f1eb1c3f62c
mancuoj <mancuoj@gmail.com>
2024-05-20 21:01:44 +0800

    chore: update

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,14 @@ import ReactDOM from 'react-dom/client'
 import { routeTree } from './routeTree.gen'
 import '~/styles/index.css'
 
+const queryClient = new QueryClient()
+const router = createRouter({ routeTree })
+
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
   }
 }
-
-const queryClient = new QueryClient()
-const router = createRouter({ routeTree })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
