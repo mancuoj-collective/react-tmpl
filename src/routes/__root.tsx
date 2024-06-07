c16b11a31c3747ddff90e87d14c536883842a8cc
mancuoj <mancuoj@gmail.com>
2024-06-07 23:40:05 +0800

    chore: update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,6 @@
 import type { QueryClient } from '@tanstack/react-query'
 import { Link, Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { ThemeToggle } from '~/components/theme-toggle'
-import { Meteors } from '~/components/ui/meteors'
-import { ScrollArea } from '~/components/ui/scroll-area'
 import { Toaster } from '~/components/ui/sonner'
 
 export const Route = createRootRouteWithContext<{
@@ -13,25 +11,20 @@ export const Route = createRootRouteWithContext<{
 
 function Root() {
   return (
-    <ScrollArea className="h-dvh">
-      <div className="flex flex-col font-sans antialiased">
-        <div className="mx-auto p-20">
-          <nav className="mb-10 flex items-center gap-3">
-            <Link to="/" className="font-bold text-foreground/60 [&.active]:text-foreground">
-              Home
-            </Link>
-            <Link to="/about" className="font-bold text-foreground/60 [&.active]:text-foreground">
-              About
-            </Link>
-            <ThemeToggle />
-          </nav>
-          <main>
-            <Outlet />
-          </main>
-        </div>
+    <div className="flex h-dvh flex-col font-sans antialiased">
+      <div className="mx-auto p-20">
+        <nav className="mb-10 flex items-center gap-3">
+          <Link to="/" className="font-bold text-foreground/60 [&.active]:text-foreground">
+            Home
+          </Link>
+          <Link to="/about" className="font-bold text-foreground/60 [&.active]:text-foreground">
+            About
+          </Link>
+          <ThemeToggle />
+        </nav>
+        <Outlet />
+        <Toaster />
       </div>
-      <Meteors />
-      <Toaster />
-    </ScrollArea>
+    </div>
   )
 }
