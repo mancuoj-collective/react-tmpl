09c01a6298b84a65f8badb9f48f537156952af04
mancuoj <mancuoj@gmail.com>
2024-10-26 19:58:16 +0800

    chore: tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { App } from './app'
 import './globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <App />
   </StrictMode>,
 )
