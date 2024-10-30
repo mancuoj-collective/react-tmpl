d3c4a51a9e722fc90dccbd12d035bafc499dac2e
mancuoj <mancuoj@gmail.com>
2024-10-31 01:56:07 +0800

    feat: mobile fix

--- src/components/layout/auth-layout.tsx
+++ src/components/layout/auth-layout.tsx
@@ -11,7 +11,7 @@ export function AuthLayout({ children, title }: AuthLayoutProps) {
     <>
       <Head title={title} />
       <div className="grid min-h-dvh grid-cols-[1fr_min(440px,calc(100%_-_64px))_1fr] gap-x-8">
-        <div className="col-[2] flex flex-col items-center pt-36">
+        <div className="col-[2] flex flex-col items-center pt-20 md:pt-36">
           <Link to="/" className="i-tabler-brand-react text-5xl" />
           <h1 className="mb-6 mt-4 text-2xl font-semibold">{title}</h1>
           {children}
