64e9cd18fa975f019c0feea29df5e4695e703397
mancuoj <mancuoj@gmail.com>
2024-10-31 01:50:55 +0800

    feat: back link

--- src/components/layout/auth-layout.tsx
+++ src/components/layout/auth-layout.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import { Head } from '../seo'
 
 interface AuthLayoutProps extends React.ComponentProps<'div'> {
@@ -10,8 +12,8 @@ export function AuthLayout({ children, title }: AuthLayoutProps) {
       <Head title={title} />
       <div className="grid min-h-dvh grid-cols-[1fr_min(440px,calc(100%_-_64px))_1fr] gap-x-8">
         <div className="col-[2] flex flex-col items-center pt-36">
-          <div className="i-tabler-brand-react text-5xl" />
-          <h1 className="mb-5 mt-4 text-2xl font-semibold">{title}</h1>
+          <Link to="/" className="i-tabler-brand-react text-5xl" />
+          <h1 className="mb-6 mt-4 text-2xl font-semibold">{title}</h1>
           {children}
         </div>
       </div>
