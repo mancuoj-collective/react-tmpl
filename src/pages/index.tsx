2fe8c52860c4d3606fefed3f910e068120cc89ed
mancuoj <mancuoj@gmail.com>
2025-07-05 14:10:34 +0800

    chore: update deps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,13 @@ export function App() {
   return (
     <div className="relative font-sans antialiased">
       <div className="flex flex-col items-center justify-center min-h-svh">
-        <div className="flex gap-2">
-          <ThemeToggle />
+        <div className="flex gap-2.5 items-center">
           <Button variant="outline" size="icon">
-            <a className="iconify carbon--logo-github size-4.5" href="https://github.com/mancuoj-collective/react-tmpl" />
+            <a href="https://github.com/mancuoj-collective/react-tmpl">
+              GitHub
+            </a>
           </Button>
+          <ThemeToggle />
         </div>
         <Toaster richColors />
         <TwScreenIndicator />
