9b8b89df3d45aecf1b1896b88345ee0a10fcd433
mancuoj <mancuoj@gmail.com>
2025-12-08 19:43:22 +0800

    chore: update deps

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { Button } from './components/ui/button'
 export function App() {
   return (
     <div className="flex flex-col items-center justify-center h-svh">
-      <Button>react-tmpl</Button>
+      <Button size="xl">react-tmpl</Button>
     </div>
   )
 }
